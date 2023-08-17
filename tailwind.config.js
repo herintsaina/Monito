8379f31ab264633d641c524142f7ea86ae4b33ea
herintsaina <herintsainarak024@gmail.com>
2023-08-17 12:33:31 +0300

    Go Monito

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index*.{html,js}",
-            "./category*.{html,js}"],
+            "./category*.{html,js}",
+            "./page/product-detail*.{html,js}"],
   theme: {
     extend: {},
   },
